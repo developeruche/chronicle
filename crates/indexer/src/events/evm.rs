8b2fdd489340bf24239167e26b767b8637bd91ee
Nwele Uchenna (developeruche) <nweleuchenna2004@gmail.com>
2024-04-16 01:26:24 +0100

    mod: query event log working fine

--- crates/indexer/src/events/evm.rs
+++ crates/indexer/src/events/evm.rs
@@ -1,13 +1,17 @@
 use alloy::{
     primitives::{Address, B256},
-    providers::Provider,
-    rpc::types::eth::{BlockNumberOrTag, Filter, Log},
+    providers::{Provider, ReqwestProvider},
+    rpc::types::eth::{BlockNumberOrTag, Filter, Log}
 };
+use futures_core::stream::Stream;
+
+
+pub type EventStream<T> = Box<dyn Stream<Item = T> + Unpin>;
 
 /// This function queries events from a specified block number
 /// `[Filter]`, having `address`, `last_block` and `event_signature` as parameters
 pub async fn query_events(
-    provider: impl Provider,
+    provider: ReqwestProvider,
     addr: Address,
     event_sig: B256,
     block_number: BlockNumberOrTag,
@@ -20,7 +24,19 @@ pub async fn query_events(
     Ok(log)
 }
 
-/// This function subscribes to events
-pub fn subscribe_events() {
-    println!("Subscribing to events");
+
+/// This creates a filter and subscribes to an event returning the event stream <T: Stream<Item = Resp> + Unpin>
+pub async fn subscribe_events(
+    provier: ReqwestProvider,
+    addr: Address,
+    event_sig: B256,
+) -> Result<EventStream<Vec<Log>>, anyhow::Error> {
+    let filter = Filter::new()
+        .address(addr)
+        .event_signature(event_sig)
+        .from_block(BlockNumberOrTag::Latest);
+    let event = provier.watch_logs(&filter).await?;
+    let stream = event.into_stream();
+
+    Ok(Box::new(stream))
 }
--- crates/indexer/src/lib.rs
+++ crates/indexer/src/lib.rs
@@ -2,3 +2,29 @@
 pub mod events;
 /// This index tx
 pub mod tx;
+
+
+
+#[cfg(test)]
+pub mod tests {
+    use alloy::{primitives::{address, b256}, providers::ProviderBuilder};
+
+    use super::*;
+
+    #[tokio::test]
+    pub async fn test_query_events_works() {
+        let rpc_url = "https://eth.merkle.io".parse().unwrap();
+        let provider = ProviderBuilder::new().on_reqwest_http(rpc_url).unwrap();
+
+        let block_num = 19664198u64;
+        let uniswap_token_address = address!("1f9840a85d5aF5bf1D1762F925BDADdC4201F984");
+        let tranfer_event_signature =
+            b256!("ddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef");
+
+        let events = events::evm::query_events(provider, uniswap_token_address, tranfer_event_signature, block_num.into()).await.unwrap();
+
+        for log in events {
+            println!("Uniswap token logs: {log:?}");
+        }
+    }
+}
